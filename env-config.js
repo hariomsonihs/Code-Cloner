624c589186612f77f9c3dc11aecd3c47021fb309
Hariom Kumar <146684140+hariomsonihs@users.noreply.github.com>
2026-04-21 01:02:36 +0530

    Add files via upload

---
+++ env-config.js
@@ -0,0 +1,11 @@
+self.__env = {
+  apiKey: "AIzaSyCkw9HIjuET67of-fWDuHMjvBddr6ooA2Y",
+  authDomain: "blogspark-9d104.firebaseapp.com",
+  projectId: "blogspark-9d104",
+  storageBucket: "blogspark-9d104.firebasestorage.app",
+  messagingSenderId: "495239379893",
+  appId: "1:495239379893:web:fb333990d40f51a473b7fc",
+  measurementId: "G-RFKCRDDZ6Y",
+  vapidKey: "BKC5LpRQPKwiyl3HhNyim_Jo8U5-4vyUUKRpd6EpODTNfp0uf9Fe4tWFZkGy7zBWyUVX2YqJMfDR9Sp57h0QnZk",
+};
+if (typeof window !== "undefined") window.__env = self.__env;
